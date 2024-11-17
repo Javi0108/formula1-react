db96ffced8047ac6def4501202d0153dbee82bfc
Javi0108 <jgarcia@eturia.es>
2024-11-17 14:56:14 +0000

    Mejoras en CSS y responsive

--- src/components/CarViewer.tsx
+++ src/components/CarViewer.tsx
@@ -1,25 +1,15 @@
-import React, { useRef  } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
-import { useFrame } from '@react-three/fiber';
-import * as THREE from 'three';
 
 const CarModel: React.FC<{ modelPath: string; position?: [number, number, number] }> = ({ modelPath, position = [0, 0, 0] }) => {
   const { scene } = useGLTF(modelPath);
-  const modelRef = useRef<THREE.Object3D>(null);
 
-//   // Hook para animar en cada frame
-//   useFrame(() => {
-//     if (modelRef.current) {
-//       modelRef.current.rotation.y += 0.003; // Rotación en el eje Y
-//     }
-//   });
-
-  return <primitive object={scene} position={position} /*ref={modelRef}*/ />;
+  return <primitive object={scene} position={position} />;
 };
 
 const CarViewer: React.FC<{ modelPath: string }> = ({ modelPath }) => (
-  <Canvas camera={{ position: [3.5, 0.5, 0] }}>
+  <Canvas camera={{ position: [0, 1, 5] }}>
     <directionalLight position={[0, 0, 0]} intensity={5} />
     <directionalLight position={[10, 0, 0]} intensity={5} />
     <directionalLight position={[-10, 0, 0]} intensity={5} />
--- src/components/Driver.tsx
+++ src/components/Driver.tsx
@@ -32,7 +32,7 @@ const Driver: React.FC = () => {
   }
 
   return (
-    <div style={{ height: "100%" }}>
+    <div style={{ height: "100%" }} id="allDriverContainer">
       <Link to={"/"}>
         <button id="backButton">
           <i className="bi bi-arrow-left-circle-fill"></i>
@@ -40,68 +40,75 @@ const Driver: React.FC = () => {
       </Link>
       {driver.map((driver) => (
         <div key={driver.position} id="driversContainer">
-          <img
-            id="driverImg"
-            src={`http://localhost:3000/pilots/imgs/${driver.Driver.driverId}.webp`}
-            alt={`Imagen de ${driver.Driver.familyName}`}
-          />
-          <div style={{ width: "50vw", height: "50vh" }}>
-            <div id="contentContainer">
-              <div id="titleContainer">
-                <div id="titles">
-                  <div>
-                    <h1 id="mainTitle">
-                      {driver.Driver.permanentNumber}{" "}
-                      <img
-                        src={`http://localhost:3000/flags/${driver.Driver.nationality}.svg`}
-                        width={50}
-                        title={driver.Driver.nationality}
-                      />
-                    </h1>
-                    <h1>
-                      {driver.Driver.givenName} {driver.Driver.familyName}
-                    </h1>
-                  </div>
+          <div
+            id="leftContainer"
+            style={{
+              backgroundImage: `url('http://localhost:3000/pilots/imgs/${driver.Driver.driverId}.webp')`,
+            }}
+          >
+            <div id="titles">
+              <div id="titlesContainer">
+                <h1 id="mainTitle">
+                  {driver.Driver.permanentNumber}{" "}
                   <img
-                    id="helmetImg"
-                    src={`http://localhost:3000/pilots/helmets/${driver.Driver.driverId}.webp`}
-                    width={170}
+                    src={`http://localhost:3000/flags/${driver.Driver.nationality}.svg`}
+                    width={50}
+                    title={driver.Driver.nationality}
                   />
-                </div>
+                </h1>
+                <h1 id="nameTitle">
+                  {driver.Driver.givenName} {driver.Driver.familyName}
+                </h1>
               </div>
-              <div id="infoContainer">
-                <table id="infoList">
-                  <tr>
-                    <td id="infoTitle">Team</td>
-                    <td id="infoDesc">{driver.Constructors[0].name}</td>
-                  </tr>
-                  <tr>
-                    <td id="infoTitle">Team nacionality</td>
-                    <td id="infoDesc">{driver.Constructors[0].nationality}</td>
-                  </tr>
-                  <tr>
-                    <td id="infoTitle">Date of birth</td>
-                    <td id="infoDesc">{driver.Driver.dateOfBirth}</td>
-                  </tr>
-                  <tr>
-                    <td id="infoTitle">Nacionality</td>
-                    <td id="infoDesc">{driver.Driver.nationality}</td>
-                  </tr>
-                  <tr>
-                    <td id="infoTitle">Wins this year</td>
-                    <td id="infoDesc">{driver.wins}</td>
-                  </tr>
-                  <tr>
-                    <td id="infoTitle">Points</td>
-                    <td id="infoDesc">{driver.points}</td>
-                  </tr>
-                  <tr>
-                    <td id="infoTitle">Position</td>
-                    <td id="infoDesc">{driver.position}º</td>
-                  </tr>
-                </table>
-              </div>
+              <img
+                id="helmetImg"
+                src={`http://localhost:3000/pilots/helmets/${driver.Driver.driverId}.webp`}
+              />
+              <ul id="infoList">
+                <li>
+                  <span id="infoTitle">
+                    Team
+                    <p>
+                      <img
+                        src={`http://localhost:3000/teams/icons/${driver.Constructors[0].constructorId}.webp`}
+                        alt={`Escudo de ${driver.Constructors[0].name}`}
+                        width={40}
+                      />
+                    </p>
+                  </span>
+                </li>
+                <li>
+                  <span id="infoTitle">
+                    Date of birth
+                    <p id="infoDesc">{driver.Driver.dateOfBirth}</p>
+                  </span>
+                </li>
+                <li>
+                  <span id="infoTitle">
+                    Nacionality
+                    <p id="infoDesc">{driver.Driver.nationality}</p>
+                  </span>
+                </li>
+                <li>
+                  <span id="infoTitle">
+                    Wins<p id="infoDesc">{driver.wins}</p>
+                  </span>
+                </li>
+                <li>
+                  <span id="infoTitle">
+                    Points<p id="infoDesc">{driver.points}</p>
+                  </span>
+                </li>
+                <li>
+                  <span id="infoTitle">
+                    Position<p id="infoDesc">{driver.position}º</p>
+                  </span>
+                </li>
+              </ul>
             </div>
+          </div>
+
+          <div id="rightContainer">
             <CarViewer
               modelPath={`http://localhost:3000/3d-Objects/${driver.Constructors[0].constructorId}.glb`}
             />
